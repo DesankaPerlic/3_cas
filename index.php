f0e4ee0e0dcd30f93792e1fac81582319e182d13
Desanka Perlic <desankaperlic86@gmail.com>
2018-06-20 19:52:26 +0200

    vezbe

--- index.php
+++ index.php
@@ -2,5 +2,20 @@
 
 
 $ime = "Desanka";
+$rezultat = str_replace("a","4","ja sam programer");
+echo $rezultat;
+$drugaRec = str_replace(["a","e"], ["4", "3"], "ja sam programer");
+echo $drugaRec;
+$malaSlova = "OVO BI TREBALO DA BUDE MALIM SLOVIMA";
+$malaSlova = strtolower($malaSlova);
+echo $malaSlova;
+
+$samoglasniciBrojevi = str_replace(["a","e","i","o"],["4","3","1","0"], "Ideja nije moja,smislili su je neki kreativni nastavnici" );
+echo $samoglasniciBrojevi;
+
+$samoglasniciBrojevi = strtoupper($samoglasniciBrojevi);
+echo $samoglasniciBrojevi;
+
+
 
 ?>
